30ae38b7aa5090dca3d4269d3c97afd947474f9f
Duskamo <dyanownz@yahoo.com>
2022-10-02 17:33:48 -0500

    finished basic functionality of converter

--- app/static/index.js
+++ app/static/index.js
@@ -2,21 +2,111 @@ $(document).ready(function() {
 	const convertVideoButton = $('#convert-video');
 	const downloadVideoButton = $("#download-video");
 	const youtubeUrlText = $('#youtube-url');
+	const videoButton = $('#video-button');
+	const audioButton = $('#audio-button');
+	const videoCard = $('#video-card');
+
+	const videoThumbnail = $('#video-thumbnail');
+	const videoTitle = $('#video-title');
+	const videoDuration = $('#video-duration');
+	const videoTable = $('#video-table');
+
+	videoCard.hide();
+
+	videoButton.on('click', function() {
+		// Set button styling
+		videoButton.addClass("active");
+		videoButton.attr("aria-pressed","true");
+		audioButton.removeClass("active");
+		audioButton.attr("aria-pressed","false");
+
+		// Update table
+		var dataString = youtubeUrlText.val();
+		$.ajax
+		({
+  		  type: "POST",
+  		  url: "/download",
+		  	contentType: "application/json",
+		  	dataType: "json",
+			  data: JSON.stringify({"url":dataString, "type":"video"}),
+	  		  success: function(html)
+	  		  {
+	     		    videoTable.html("");
+	     		    for (let i = 0; i < html.videos.length; i++) {
+	     		    	videoTable.append(
+		     		    	"<tr>" +
+				          "<td>" + html.videos[i].resolution + "</td>" +
+				          "<td>" + html.videos[i].size + "mb</td>" +
+				          '<td><button type="button" class="btn btn-primary"><i class="fa-solid fa-video"></i> Download</button></td>' +
+				        "</tr>"
+				        );
+	     		    }
+	  		  }
+			});
+	});
+
+	audioButton.on('click', function() {
+		// Set button styling
+		audioButton.addClass("active");
+		audioButton.attr("aria-pressed","true");
+		videoButton.removeClass("active");
+		videoButton.attr("aria-pressed","false");
+
+		// Update table
+		var dataString = youtubeUrlText.val();
+		$.ajax
+		({
+  		  type: "POST",
+  		  url: "/download",
+		  	contentType: "application/json",
+		  	dataType: "json",
+			  data: JSON.stringify({"url":dataString, "type":"audio"}),
+	  		  success: function(html)
+	  		  {
+	     		    videoTable.html("");
+	     		    for (let i = 0; i < html.videos.length; i++) {
+	     		    	videoTable.append(
+		     		    	"<tr>" +
+				          "<td>" + html.videos[i].resolution + "</td>" +
+				          "<td>" + html.videos[i].size + "mb</td>" +
+				          '<td><button type="button" class="btn btn-primary"><i class="fa-solid fa-headphones"></i> Download</button></td>' +
+				        "</tr>"
+				        );
+	     		    }
+	  		  }
+			});
+	});
 
 	convertVideoButton.on('click', function() {
+		videoCard.show();
+
 		var dataString = youtubeUrlText.val();
 
 		$.ajax
 		({
   		  type: "POST",
   		  url: "/download",
-		  contentType: "application/json",
-		  data: JSON.stringify({"data":dataString}),
-  		  success: function(html)
-  		  {
-     		    alert(html);
-  		  }
-		});
+		  	contentType: "application/json",
+		  	dataType: "json",
+			  data: JSON.stringify({"url":dataString, "type":"video"}),
+	  		  success: function(html)
+	  		  {
+	  		  		videoThumbnail.attr("src",html.thumbnail_url);
+	     		    videoTitle.html(html.title);
+	     		    videoDuration.html(html.length);
+
+	     		    videoTable.html("");
+	     		    for (let i = 0; i < html.videos.length; i++) {
+	     		    	videoTable.append(
+		     		    	"<tr>" +
+				          "<td>" + html.videos[i].resolution + "</td>" +
+				          "<td>" + html.videos[i].size + "mb</td>" +
+				          '<td><button type="button" class="btn btn-primary"><i class="fa-solid fa-video"></i> Download</button></td>' +
+				        "</tr>"
+				        );
+	     		    }
+	  		  }
+			});
 	});
 
 	downloadVideoButton.on("click", function() {
--- youtubeconverter_main.py
+++ youtubeconverter_main.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, jsonify
 
 import os
-import sys
+from sys import platform
 from pytube import YouTube
+from pathlib import Path
 
 app = Flask(__name__,
             template_folder="app/templates",
             static_url_path="",
             static_folder="app/static")
 
+# ############ HELPER METHODS ############
+def bytesto(bytes, to, bsize=1024): 
+    a = {'k' : 1, 'm': 2, 'g' : 3, 't' : 4, 'p' : 5, 'e' : 6 }
+    r = float(bytes)
+    return bytes / (bsize ** a[to])
+
+
+# ############ REST METHODS ############
 @app.route("/", methods=['GET'])
 def hello():
     return render_template('index.html')
@@ -20,10 +29,40 @@ def download():
         link = request.get_json()
 
         # Download youtube video to server
-        yt = YouTube(link['data'])
-        #yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download(os.path.expanduser('~/Downloads'))
+        yt = YouTube(link['url'])
+        videos = None
+        
+        if link['type'] == 'video':
+            videos = yt.streams.filter(only_video=True, file_extension='mp4').order_by('resolution').desc()
+        elif link['type'] == 'audio':
+            videos = yt.streams.filter(only_audio=True).order_by('abr').desc()
 
+        data = {
+            'title': yt.title,
+            'thumbnail_url': yt.thumbnail_url,
+            'length': yt.length,
+            'videos': []
+        }
+
+        if link['type'] == 'video':
+            for i in range(len(videos)):
+                data['videos'].append({'resolution': videos[i].resolution, 'size': round(bytesto(videos[i].filesize,'m')), 'id': i})
+        elif link['type'] == 'audio':
+            for i in range(len(videos)):
+                data['videos'].append({'resolution': videos[i].abr, 'size': round(bytesto(videos[i].filesize,'m')), 'id': i})
+
+
+        return jsonify(data)
+
+        
+        # First Method
+        # downloads_path = str(Path.home() / "Downloads")
+        # if platform == "win32":
+        #     video.download(downloads_path)
+        # else:
+        #     video.download('~/Downloads')
+
+        # Second Method
         # Send video to correct folder
         #os.rename(os.getcwd() + "/" + "{0}.mp4".format(yt.title), os.getcwd() + "/videos/{0}.mp4".format(yt.title))
 
